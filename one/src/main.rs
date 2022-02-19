a3f3571566855546618b181aca78cab484cc444a
karrybit <takumi.k.5610@gmail.com>
2022-02-19 12:18:14 +0900

    request two from one

--- one/src/main.rs
+++ one/src/main.rs
@@ -1,26 +1,39 @@
-use axum::{routing::get, Router};
-use std::net::SocketAddr;
-
 #[tokio::main]
 async fn main() {
     // initialize tracing
     tracing_subscriber::fmt::init();
 
     // build our application with a route
-    let app = Router::new()
+    let app = axum::Router::new()
         // `GET /` goes to `root`
-        .route("/", get(hello));
+        .route("/", axum::routing::get(hello));
 
     // run our app with hyper
     // `axum::Server` is a re-export of `hyper::Server`
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    tracing::debug!("listening on {}", addr);
+    let addr = std::net::SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::info!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
 }
 
-async fn hello() -> &'static str {
-    "hello from one service"
+async fn hello() -> String {
+    println!("requested hello in one");
+    tracing::info!("requested hello in one");
+    let response = match reqwest::get("http://localhost:4000/").await {
+        Ok(r) => r,
+        Err(e) => {
+            println!("{:?}", e);
+            panic!()
+        }
+    };
+    let response = match response.text().await {
+        Ok(r) => r,
+        Err(e) => {
+            println!("{:?}", e);
+            panic!()
+        }
+    };
+    format!("hello from one service\n{}\n", response)
 }
--- two/src/main.rs
+++ two/src/main.rs
@@ -1,20 +1,17 @@
-use axum::{routing::get, Router};
-use std::net::SocketAddr;
-
 #[tokio::main]
 async fn main() {
     // initialize tracing
     tracing_subscriber::fmt::init();
 
     // build our application with a route
-    let app = Router::new()
+    let app = axum::Router::new()
         // `GET /` goes to `root`
-        .route("/", get(hello));
+        .route("/", axum::routing::get(hello));
 
     // run our app with hyper
     // `axum::Server` is a re-export of `hyper::Server`
-    let addr = SocketAddr::from(([127, 0, 0, 1], 4000));
-    tracing::debug!("listening on {}", addr);
+    let addr = std::net::SocketAddr::from(([127, 0, 0, 1], 4000));
+    tracing::info!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
@@ -23,5 +20,7 @@ async fn main() {
 
 // basic handler that responds with a static string
 async fn hello() -> &'static str {
+    println!("requested hello in two");
+    tracing::info!("requested hello in two");
     "hello from two service"
 }
